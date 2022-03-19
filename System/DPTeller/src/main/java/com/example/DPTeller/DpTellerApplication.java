89f4ab45201c057d9affc51b5ac192f3985c96ae
tridsanu taychathunyachot <tridsanu.tay@gmail.com>
2022-03-20 01:25:04 +0700

    add service2

---
+++ System/DPTeller/src/main/java/com/example/DPTeller/DpTellerApplication.java
@@ -0,0 +1,13 @@
+package com.example.DPTeller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpTellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpTellerApplication.class, args);
+	}
+
+}
---
+++ System/DPTeller/src/test/java/com/example/DPTeller/DpTellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DPTeller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpTellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
