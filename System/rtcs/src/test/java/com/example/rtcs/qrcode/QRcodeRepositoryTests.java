56859eeb200753d3f3a62e77da81261ce80b032b
tridsanu taychathunyachot <tridsanu.tay@gmail.com>
2022-03-20 01:09:27 +0700

    testing for QRcode

---
+++ System/rtcs/src/test/java/com/example/rtcs/qrcode/QRcodeRepositoryTests.java
@@ -0,0 +1,31 @@
+package com.example.rtcs.qrcode;
+
+
+import com.example.rtcs.qrcode.entity.QRcode;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+public class QRcodeRepositoryTests {
+    @Autowired
+    private  QRcodeRepository qRcodeRepository;
+
+    @Test
+    void findById_with_success_Case() {
+        // Arrange
+        QRcode qRcode = new QRcode();
+        qRcode.setId(1L);
+        qRcode.setSender("TestSender");
+        qRcode.setReceiver("TestReceiver");
+        qRcodeRepository.save(qRcode);
+        // Act
+        Optional<QRcode> result = qRcodeRepository.findById(1L);
+        // Assert
+        assertTrue(result.isPresent());
+    }
+}
